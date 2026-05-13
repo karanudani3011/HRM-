29a9a1d4e267200c918b2172ba9f7cbe04eba5a4
Jeel Dave <jeeldave8gmail.com>
2026-05-13 11:29:43 +0530

    dashboard update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import PostCarousel from '../components/PostCarousel';
 import ServicePortals from '../components/ServicePortals';
 import SampleShowcase from '../components/SampleShowcase';
 import UIMockups from '../components/UIMockups';
@@ -8,7 +7,6 @@ import UIMockups from '../components/UIMockups';
 const Home = () => {
   return (
     <main>
-      <PostCarousel />
       <Hero />
       <ServicePortals />
       <SampleShowcase />
